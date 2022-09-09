f6d08695bf28757590229cb781ba6fbb440ef670
Svetlana Tokarchuk <lanawm73@gmail.com>
2022-09-09 06:53:28 -0400

    Files added

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+
+public class Main {
+    public static void main(String[] args) {
+        List<Person> people = new ArrayList<>();
+        people.add(new Person("Petr", "Ivanov", 25));
+        people.add(new Person("Oleg", "Petrov", 40));
+        people.add(new Person("Katerina", "Volkova", 18));
+
+        PersonComparator personComparator = new PersonComparator(7);
+        //people.sort(personComparator);
+        Collections.sort(people, personComparator);
+        System.out.println("Отсортированный список: " + people);
+    }
+
+}
