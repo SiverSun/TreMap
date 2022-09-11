b19a8def369e0bfb55175c947baa816700778309
Svetlana Tokarchuk <lanawm73@gmail.com>
2022-09-11 08:15:33 -0400

    Change lambda

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,4 @@
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
 
@@ -7,11 +6,13 @@ import java.util.List;
 public class Main {
     public static void main(String[] args) {
         List<Person> people = new ArrayList<>();
-        people.add(new Person("Petr", "Ivanov", 25));
-        people.add(new Person("Oleg", "Petrov", 40));
+        people.add(new Person("Petr", "Ivanov Rolikov", 25));
+        people.add(new Person("Oleg", "Petrov Korsak Mladshiy", 40));
         people.add(new Person("Katerina", "Volkova", 18));
 
-        people.sort((Person o1, Person o2) -> o1.getSurname().compareTo(o2.getSurname()));
+        Comparator<Person> comparator = (p1, p2) -> p1.getSurname().length() - p2.getSurname().length();
+        people.sort(comparator.reversed());
+
         System.out.println("Отсортированный список: " + people);
     }
 
